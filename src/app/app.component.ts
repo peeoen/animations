a35d8bbde0833bbb99ab143f407eb16132f11b14
peeoen <theerawat.pon@gmail.com>
2018-03-13 01:35:44 +0700

    Add AOS animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit  {
+
+  data:string[] = []
+  constructor(){
+  
+  }
+
+  ngOnInit(){
+    AOS.init();
+
+    for(let i=1;i<=50;i++){
+      this.data.push(i.toString())
+    }
+  }
 }
